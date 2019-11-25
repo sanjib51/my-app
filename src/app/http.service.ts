d92f37707e28b01ea21fcc44ead72b75fee91063
Sanjib <sanjib51@gmail.com>
2019-11-26 00:37:36 +0530

    Update http.service.ts

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -9,7 +9,7 @@ export class HttpService {
 
   constructor(private http:HttpClient) { }
   getBeer(){
-    return this.http.get('http://sanjib51.pythonanywhere.com/api/v1/word/10/1'); 
+    return this.http.get('http://sanjib51.pythonanywhere.com/api/v1/word/10/101'); 
    /* return this.http.get('https://api.openbrewerydb.org/breweries'); */
 
   }
